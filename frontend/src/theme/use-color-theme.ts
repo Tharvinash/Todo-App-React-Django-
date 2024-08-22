7b0d1fa5d8e62acfea982102f24a5d1724eaa285
Tharvinash <tharvinjr24@hmail.com>
2024-08-22 13:42:58 +0800

    Added theme colors

--- frontend/src/theme/use-color-theme.ts
+++ frontend/src/theme/use-color-theme.ts
@@ -1,6 +1,6 @@
 import { createTheme, PaletteMode } from '@mui/material';
 import React from 'react';
-import theme, { getDesignTokens } from './theme';
+import { getDesignTokens } from './theme';
 
 export const useColorTheme = () => {
   const [mode, setMode] = React.useState<PaletteMode>('light');
@@ -11,22 +11,10 @@ export const useColorTheme = () => {
   };
 
   const modifiedTheme = React.useMemo(
-    () =>
-      createTheme({
-        ...theme,
-        palette: {
-          ...theme.palette,
-          mode,
-        },
-      }),
+    () => createTheme(getDesignTokens(mode)),
     [mode]
   );
 
-  // const modifiedTheme = React.useMemo(
-  //   () => createTheme(getDesignTokens(mode)),
-  //   [mode]
-  // );
-  console.log('FIRED', mode);
   return {
     theme: modifiedTheme,
     mode,
